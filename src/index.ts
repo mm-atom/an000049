3da51c23c05e131f7cd0063f9923496c762ab6d0
taoqf <tao_qiufeng@126.com>
2022-04-27 16:45:33 +0800

    add config Option

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,23 @@ import knex, { Knex } from 'knex';
 import anylogger from 'anylogger';
 import config from '@mmstudio/config';
 
+
+export type Transaction = Knex.Transaction;
+export type Config = Knex.Config;
+
 const logger = anylogger('@mmstudio/an000049');
-const db = config.dbconfig as Knex.Config;
+const db = config.dbconfig as Config;
 
 const pg = knex({
+	// debug: process.env.NODE_ENV === 'development',	// Can we do this?
 	debug: true,
 	...db,
 	log: logger
 });
 
-export default function an49() {
-	return pg;
+export default function an49(config?: Config) {
+	if (!config) {
+		return pg;
+	}
+	return knex(config);
 }
