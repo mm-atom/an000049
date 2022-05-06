9f7948a509a0bd9cef4edeae902eaf3b916dac91
taoqf <tao_qiufeng@126.com>
2022-05-06 11:10:15 +0800

    add export type

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,45 @@ import config from '@mmstudio/config';
 export type Transaction = Knex.Transaction;
 export type Config = Knex.Config;
 
+// From knex
+// Container type for situations when we want a partial/intersection eventually
+// but the keys being selected or additional properties being augmented are not
+// all known at once and we would want to effectively build up a partial/intersection
+// over multiple steps.
+type DeferredKeySelection<
+	// The base of selection. In intermediate stages this may be unknown.
+	// If it remains unknown at the point of resolution, the selection will fall back to any
+	TBase,
+	// Union of keys to be selected
+	// In intermediate stages this may be never.
+	TKeys extends string,
+	// Changes how the resolution should behave if TKeys is never.
+	// If true, then we assume that some keys were selected, and if TKeys is never, we will fall back to any.
+	// If false, and TKeys is never, then we select TBase in its entirety
+	THasSelect extends true | false = false,
+	// Mapping of aliases <key in result> -> <key in TBase>
+	TAliasMapping extends Record<string, unknown> = Record<string, never>,
+	// If enabled, then instead of extracting a partial, during resolution
+	// we will pick just a single property.
+	TSingle extends boolean = false,
+	// Extra props which will be intersected with the result
+	TIntersectProps extends Record<string, unknown> = Record<string, never>,
+	// Extra props which will be unioned with the result
+	TUnionProps = never
+	> = {
+		// These properties are not actually used, but exist simply because
+		// typescript doesn't end up happy when type parameters are unused
+		_base: TBase;
+		_hasSelection: THasSelect;
+		_keys: TKeys;
+		_aliases: TAliasMapping;
+		_single: TSingle;
+		_intersectProps: TIntersectProps;
+		_unionProps: TUnionProps;
+	};
+
+export type Callback<T> = (qb: Knex.QueryBuilder<T, DeferredKeySelection<T, never, false, Record<string, never>, false, Record<string, never>, never>[]>) => unknown;
+
 const logger = anylogger('@mmstudio/an000049');
 const db = config.dbconfig as Config;
 
